1b99dc10bb950b453f46ea06c6aab3455df1811a
guy arbily <guy.arbilly@gmail.com>
2019-06-14 17:10:49 +0100

    final version - after adding enter key and hiding the red X- will appear only whn hover on row

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var app = angular.module("todosApp", []);
+
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
--- todosCtrl.js
+++ todosCtrl.js
@@ -1,14 +1,33 @@
 app.controller("todosCtrl", function($scope){
 
+    $scope.stateQuery = -1;
+    
+
     $scope.todoArr = [];
     function Todo(text, done) {
         this.text = text;
         this.done = done;
     }
 
-
+    
     $scope.todoArr.push(new Todo("Learn Angular", false));
     $scope.todoArr.push(new Todo("Take Noa homer", true));
+
+    
+    
+    $scope.setFilterState = function(value) {
+        $scope.stateQuery = value;
+    }
+
+    $scope.filterByState = function(item){
+        if ($scope.stateQuery == -1) {
+            // if want to see all entries - return true in anycase
+            return true;
+        } else {
+            return (($scope.stateQuery && item.done) || (!$scope.stateQuery && !item.done))
+        }
+    }
+  
 
     
     $scope.total = function() {
@@ -34,6 +53,39 @@ app.controller("todosCtrl", function($scope){
         $scope.newItem = "";
     }
 
+    $scope.cleanItem = function(item) {
+        
+        if (!item.done) {
+            // set message and message ind
+            if (confirm('ToDo item is not completed. Are you sure you want to delete it?')) {
+                // continue to delete it!
+            } else {
+                return;
+            }
+        }
+            
+        
+        // go over the todos and look for the item
+        let index = -1;
+        for( let i=0, len= $scope.todoArr.length; i<len; i++){
+            if ($scope.todoArr[i] == item){
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            console.error("cleanItem() faied to find item");
+            return;
+        } else {
+            // check if the item is still not done
+            
+            $scope.todoArr.splice(index, 1);
+        }
+        // reset to show all
+        $scope.stateQuery=-1;
+        
+    }
+
     $scope.cleanDoneItems = function() {
         let tempArr = [];
         // go over the list and copy the once that are not yet done
@@ -45,9 +97,15 @@ app.controller("todosCtrl", function($scope){
         }
         // override the todoArr with temp array
         $scope.todoArr = tempArr;
+        // reset to show all
+        $scope.stateQuery=-1;
     }
 
     $scope.removeAll = function() {
         $scope.todoArr = [];
+        // reset to show all
+        $scope.stateQuery=-1;
     }
+
+    
 });
